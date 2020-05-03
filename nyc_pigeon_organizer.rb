4130e9dac52b7ad0c891ca89493e24d097ad48bf
Austin Evans <unity7591@gmail.com>
2020-05-03 17:11:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,11 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |color_gender_lives, value|
-    value.each do |stats, all_names|
-      all_names.each do |name|
-        if new_hash[name] == nil 
-          new_hash[name] = {}
-        end
-        if new_hash[name][color_gender_lives] == nil
-          new_hash[name][color_gender_lives] = {}
-        end
-        stats.to_s
+    value.each do |gender, names|
+      names.each do |name|
+        new_hash[name] || = {}
+        new_hash[name][color_gender_lives] || = {}
+        new_hash[name][color_gender_lives] << gender.to_s
       end
     end
   end
@@ -23,7 +19,12 @@ end
 
 
 
-
+# if new_hash[name] == nil 
+ #         new_hash[name] = {}
+  #      end
+   #     if new_hash[name][color_gender_lives] == nil
+    #      new_hash[name][color_gender_lives] = {}
+     #   end
 
 
 
