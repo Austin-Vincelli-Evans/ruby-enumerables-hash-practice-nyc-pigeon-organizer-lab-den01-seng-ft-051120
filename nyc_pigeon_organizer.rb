63c81c2d9e96ae5ec712d9a372b33de92f5b4559
Austin Evans <unity7591@gmail.com>
2020-05-03 17:24:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,31 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |color_gender_lives, value|
-    value.each do |gender, names|
-      names.each do |name|
-        new_hash[name] ||= {}
-        new_hash[name][color_gender_lives] ||= {}
-        new_hash[name][color_gender_lives] << gender.to_s
+    value.each do |value, array|
+      array.each do |name|
+       if new_hash[name] == nil 
+          new_hash[name] = {}
+          new_hash[name][color_gender_lives] = []
+        else
+          new_hash[name][color_gender_lives] = []
+        end
       end
     end
   end
-  return new_hash
+  new_hash.each do |name, value|
+    value.each do |hashvalue, array|
+      data.each do |color_gender_lives, value|
+        value.each do |value, array|
+          array.each do |element|
+            if element == name && hashvalue == color_gender_lives
+              new_hash[name][hashvalue] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  new_hash
 end
  
 
@@ -26,7 +42,9 @@ end
     #      new_hash[name][color_gender_lives] = {}
      #   end
 
-
+#new_hash[name] ||= {}
+ #       new_hash[name][color_gender_lives] ||= {}
+  #      new_hash[name][color_gender_lives] << gender.to_s
 
 #{:name =>{:color =>[], :gender =>[], :lives => [],}}
  # value == "Theo" || "Peter Jr" || "Lucky" || "Ms K" || "Queenie" || "Alex" || "Andrew"
